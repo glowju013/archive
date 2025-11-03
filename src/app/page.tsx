7af7c14a153c0024f73cceab93326e8e481234d8
glowju013 <nevertheless113@naver.com>
2025-11-03 16:44:44 +0900

    refactor: use server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-'use client';
-
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect("/plants");
+  redirect('/plants')
 }
